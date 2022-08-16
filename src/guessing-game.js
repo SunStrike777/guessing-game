de77f04ddf20be782d43d517c436f4cc0e694f79
SunStrike777 <34217160+SunStrike777@users.noreply.github.com>
2022-08-17 00:35:35 +0500

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
+
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
-    setRange(min, max) {
-
+    setRange(minValue, maxValue) {
+        this.minValue = minValue;
+        this.maxValue = maxValue;
     }
 
     guess() {
-
+        this.result = Math.round((this.minValue + this.maxValue) / 2)
+        return this.result
     }
 
     lower() {
-
+        this.maxValue = this.result
+        this.guess()
     }
 
     greater() {
-
+        this.minValue = this.result
+        this.guess()
     }
 }
 
